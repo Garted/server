334c139ba4941cf9661bbe15e3680708e3f41fd4
Eugeny Kurnysh <garted93@gmail.com>
2024-06-10 12:50:02 +0300

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+// Маршрут для получения данных о персонаже по ID
+app.get("/character/2", async (req, res) => {
+    try {
+        const response = await axios.get(
+            `https://rickandmortyapi.com/api/character/2`
+        );
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching data from API:", error);
+        res.status(500).json({ error: "Error fetching data from API" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
